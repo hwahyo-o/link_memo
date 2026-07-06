d91482b2a3b40153d17074b2e69c0a6c30688b04
hwahyo-o <ajas03974@gmail.com>
2026-07-06 17:46:46 +0900

    Remove exposed Firebase fallback config

--- src/services/firebase-client.js
+++ src/services/firebase-client.js
@@ -18,26 +18,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, onSnapshot, deleteDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-const fallbackConfig = {
-    apiKey: "AIzaSyBJFs1rgUPZqjwZt2wgNuKBXH3uxDpZFXc",
-    authDomain: "link-note-c8c1d.firebaseapp.com",
-    projectId: "link-note-c8c1d",
-    storageBucket: "link-note-c8c1d.firebasestorage.app",
-    messagingSenderId: "993879795668",
-    appId: "1:993879795668:web:f1401e2c4da1c6cc50d841",
-    measurementId: "G-C468ZCLCWH"
-};
-
 const viteEnv = import.meta.env || {};
 
 const envConfig = {
-    apiKey: viteEnv.VITE_FIREBASE_API_KEY || fallbackConfig.apiKey,
-    authDomain: viteEnv.VITE_FIREBASE_AUTH_DOMAIN || fallbackConfig.authDomain,
-    projectId: viteEnv.VITE_FIREBASE_PROJECT_ID || fallbackConfig.projectId,
-    storageBucket: viteEnv.VITE_FIREBASE_STORAGE_BUCKET || fallbackConfig.storageBucket,
-    messagingSenderId: viteEnv.VITE_FIREBASE_MESSAGING_SENDER_ID || fallbackConfig.messagingSenderId,
-    appId: viteEnv.VITE_FIREBASE_APP_ID || fallbackConfig.appId,
-    measurementId: viteEnv.VITE_FIREBASE_MEASUREMENT_ID || fallbackConfig.measurementId
+    apiKey: viteEnv.VITE_FIREBASE_API_KEY,
+    authDomain: viteEnv.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: viteEnv.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: viteEnv.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: viteEnv.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: viteEnv.VITE_FIREBASE_APP_ID,
+    measurementId: viteEnv.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 let config = envConfig;
